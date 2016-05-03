9694001cbccc51b034cc31ae65e1deb6706f8f77
fangrui <fangrui@bilibili.com>
2016-05-03 20:01:58 +0800

    update

--- main.cc
+++ main.cc
@@ -2,34 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/at_exit.h"
-#include "base/location.h"
-#include "base/message_loop/message_loop.h"
-#include "base/run_loop.h"
-#include "base/strings/string_number_conversions.h"
-#include "base/files/file_util.h"
-#include "base/time/time.h"
-#include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/threading/sequenced_worker_pool.h"
+
 
 int main(int argc, char* argv[]) {
-  base::AtExitManager exit_manager;
-  if (argc <= 1) {
-    LOG(INFO) << argv[0] << ": missing operand";
-    return -1;
-  }
+  scoped_ptr<base::SequencedWorkerPool> my_pool(new base::SequencedWorkerPool(5, "fangr_"));
 
-  int duration_seconds = 0;
-  if (!base::StringToInt(argv[1], &duration_seconds) ||
-      duration_seconds < 0) {
-    LOG(INFO) << argv[0] << ": invalid time interval '" << argv[1] << "'";
-    return -1;
-  }
 
-  base::TimeDelta duration = base::TimeDelta::FromSeconds(duration_seconds);
-  base::MessageLoop main_loop;
-  base::RunLoop run_loop;
-  main_loop.PostDelayedTask(FROM_HERE, run_loop.QuitClosure(), duration);
-  run_loop.Run();
 
   return 0;
 }
